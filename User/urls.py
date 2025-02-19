7b3b51dc8428651cfe7120cef8c9c46c01687f5c
AslanBe1 <aslanabdimuminov31@gmail.com>
2025-02-19 21:58:58 +0500

    Send Email And Code

--- User/urls.py
+++ User/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login-oauth-page/', views.login_page, name='login_page'),
     path('logout/', views.logout_page, name='logout_page'),
     path('register/', views.register_page, name='register_page'),
+    path('enter-number', views.verify_code,name='verify_code'),
 ]
--- User/views.py
+++ User/views.py
@@ -1,11 +1,17 @@
+import random
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail ,EmailMessage
 
+from User.models import User
+from config.settings import EMAIL_HOST_USER
 from User.forms import LoginForm, RegisterForm
 
 
 # Create your views here.
+def generate_verification_code():
+    return str(random.randint(100000,999999))
 
 def login_page(request):
     form = LoginForm()
@@ -41,14 +47,56 @@ def register_page(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
+            get_name_by_email = user.email.split('@')[0]
             user.is_staff = True
             user.is_superuser = True
             user.set_password(user.password)
-            user.save()
-            return redirect('shops:index')
+            user.set_password(form.cleaned_data['password'])
+            verification_code = generate_verification_code()
+            request.session['verification_code'] = verification_code
+
+            request.session['user_data'] = {
+                'email': user.email,
+                'password': form.cleaned_data['password'],
+                'is_staff': user.is_staff,
+                'is_superuser': user.is_superuser,
+            }
+            send_mail(
+                f'{get_name_by_email}',
+                f'{get_name_by_email}\'s account has been successfully created\nYour Code {verification_code}',
+                EMAIL_HOST_USER,
+                [user.email],
+                fail_silently=False,
+            )
+
+            return redirect('user:verify_code')
 
     context = {
         'form': form,
     }
 
     return render(request, 'User/register.html', context=context)
+
+
+def verify_code(request):
+    if request.method == 'POST':
+        entered_code = request.POST.get('code')
+        stored_code = request.session.get('verification_code')
+        user_data = request.session.get('user_data')
+
+        if entered_code == stored_code:
+            del request.session['verification_code']
+            del request.session['user_data']
+            user = User.objects.create_user(
+                email=user_data['email'],
+                password=user_data['password'],
+                is_staff=user_data['is_staff'],
+                is_superuser=user_data['is_superuser']
+            )
+            login(request, user)
+            messages.add_message(request, messages.SUCCESS, 'Your Code has been verified')
+            return redirect('shops:index')
+        else:
+            messages.add_message(request, messages.ERROR, 'Invalid code')
+
+    return render(request, 'User/enter_number.html')
